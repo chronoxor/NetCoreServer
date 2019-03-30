163ea31d8b1a6f115f092c66f53c774ab542bac9
Ivan Shynkarenka <chronoxor@gmail.com>
2019-03-30 19:10:50 +0300

    Build script with /t:pack command

--- build/VisualStudio/01-build.bat
+++ build/VisualStudio/01-build.bat
@@ -1,5 +1,5 @@
 cd ../..
 nuget restore NetCoreServer.sln
-MSBuild NetCoreServer.sln /p:Configuration=Release
+MSBuild NetCoreServer.sln /p:Configuration=Release /t:pack
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd build/VisualStudio
