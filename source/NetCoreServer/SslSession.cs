ae86bd7144dfb4c223c2cec02b92c38e8802f039
Ivan Shynkarenka <chronoxor@gmail.com>
2019-11-09 21:44:11 +0300

    .NET Core 2.0

--- source/NetCoreServer/SslSession.cs
+++ source/NetCoreServer/SslSession.cs
@@ -159,14 +159,6 @@ namespace NetCoreServer
 
             try
             {
-                try
-                {
-                    // Shutdown the SSL stream
-                    _sslStream.ShutdownAsync().Wait();
-                }
-                catch (AggregateException) {}
-                catch (IOException) {}
-
                 // Dispose the SSL stream & buffer
                 _sslStream.Dispose();
                 _sslStreamId = null;
