0b9ba5c0b22ad30891c6078f94aa6a655c6796d1
Ivan Shynkarenka <chronoxor@gmail.com>
2020-09-07 03:07:40 +0300

    build

--- tests/WssTests.cs
+++ tests/WssTests.cs
@@ -142,7 +142,7 @@ namespace tests
             Assert.True(client.BytesReceived > 0);
             Assert.True(!client.Errors);
         }
-/*
+
         [Fact(DisplayName = "WebSocket secure server multicast test")]
         public void WssServerMulticastTest()
         {
@@ -254,7 +254,7 @@ namespace tests
             Assert.True(!client2.Errors);
             Assert.True(!client3.Errors);
         }
-*/
+/*
         [Fact(DisplayName = "WebSocket secure server random test")]
         public void WssServerRandomTest()
         {
@@ -381,5 +381,6 @@ namespace tests
             Assert.True(server.BytesReceived > 0);
             Assert.True(!server.Errors);
         }
+*/
     }
 }
